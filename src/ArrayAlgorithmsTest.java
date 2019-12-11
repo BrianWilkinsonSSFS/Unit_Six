5c238fc50db03c74460d484bf1ac1e099b4c576b
wilkinsonbrian <wilkibrian@gmail.com>
2019-12-11 14:47:25 -0500

    Changed assertEquals to assertTrue for boolean tests.

--- src/ArrayAlgorithmsTest.java
+++ src/ArrayAlgorithmsTest.java
@@ -14,14 +14,15 @@ public class ArrayAlgorithmsTest {
 
     @Test
     public void checkForAllNegatives() {
-        assertEquals(true, ArrayAlgorithms.checkForAllNegatives(new int[] {-2, -9, -6, -8, -9, -3, -1}));
-        assertEquals(false, ArrayAlgorithms.checkForAllNegatives(new int[] {-2, -9, -6, -8, -9, -3, 0}));
+        assertTrue(ArrayAlgorithms.checkForAllNegatives(new int[] {-2, -9, -6, -8, -9, -3, -1}));
+        assertFalse(ArrayAlgorithms.checkForAllNegatives(new int[] {-2, -9, -6, -8, -9, -3, 0}));
+
     }
 
     @Test
     public void hasDupes() {
-        assertEquals(true, ArrayAlgorithms.hasDupes(new int[] {5, 23, 7, 14, 39, 46, 52, 65, 75, 83, 76, 44, 90, 6, 2, 5}));
-        assertEquals(false, ArrayAlgorithms.hasDupes(new int[] {5, 23, 7, 14, 39, 46, 52, 65, 75, 83, 76, 44, 90, 6, 2, 11}));
+        assertTrue(ArrayAlgorithms.hasDupes(new int[] {5, 23, 7, 14, 39, 46, 52, 65, 75, 83, 76, 44, 90, 6, 2, 5}));
+        assertFalse(ArrayAlgorithms.hasDupes(new int[] {5, 23, 7, 14, 39, 46, 52, 65, 75, 83, 76, 44, 90, 6, 2, 11}));
     }
 
     @Test
